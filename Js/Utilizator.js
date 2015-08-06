a2bd201eae16c7909d308e972d32448695b93234
bogdanfazakas <bogdan.fazakas@gmail.com>
2015-08-06 10:01:54 +0300

    Task-Validations
    In this task I've implmemented validation for the new question page in
    the admin view and changed data validation from home page , all this
    were implemented using JqueryValidation plugin for Jquery.

--- Js/Utilizator.js
+++ Js/Utilizator.js
@@ -3,12 +3,11 @@ var firstName;
 var lastName;
 var userEmail;
 var i;
-var formular;
+var form;
 
 $(document).ready(function(){
     resetCookie();
-    formular=$("#userForm");
-    formular.validate();
+    validateForm();
     $("#next").click(function(){
 		userCheck();
     });
@@ -37,6 +36,34 @@ function salvareIndexUtilizator(){
 	}
 }
 
+function validateForm(){
+	
+	jQuery.validator.setDefaults({
+  		debug: true,
+  		success: "valid"
+	});
+	
+	form = $("#userForm");
+	
+	form.validate({
+  		rules: {
+    		lastname: {
+    			required: true,
+    			minlength : "3",
+    			pattern: "[a-zA-Z]+"
+    		},
+    		firstname : {
+    			required: true,
+    			minlength : "3",
+    			pattern:"[a-zA-Z]+"
+   			 },
+   			 email : {
+    			required: true,
+    			email : true
+    		}
+  		}
+	});
+}
 
 function resetCookie(){
 	setCookieValue("min","15");
@@ -45,13 +72,17 @@ function resetCookie(){
 	setCookieValue("firstName","");
 	setCookieValue("lastName","");
 	setCookieValue("userEmail","");
+	
 	for (var i = 1; i <= 10; i++) {
 		setCookieValue("rez"+i.toString(),"0,0,0,0,0");
 	}
 }
 
 function userCheck() {
-	if (formular.valid() == true ) {
+	
+	validateForm();
+	
+	if (form.valid() == true ) {
 		salveazaUtilizator();
 		salvareIndexUtilizator();
 		location.href='Html/Intrebarea1.html';
@@ -59,7 +90,10 @@ function userCheck() {
 }
 
 function adminCheck() {
-	if (formular.valid() == true ) {
+	
+	validateForm();
+
+	if (form.valid() == true ) {
 		salveazaUtilizator();
 		location.href='Html/AdminPage.html';
 	}
